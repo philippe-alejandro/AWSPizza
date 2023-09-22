f0847926753eb898a4928958bd7b65a35e7ed6b6
Philippe Brenner Roman <philippebrennerroman@Philippes-MacBook-Pro.local>
2023-09-22 08:50:59 -0600

    Initial commit

--- the-state-machine/typescript/lambda-fns/orderPizza.ts
+++ the-state-machine/typescript/lambda-fns/orderPizza.ts
@@ -1,11 +1,121 @@
-exports.handler = async function(flavour:any) {
-    console.log("Requested Pizza :", JSON.stringify(flavour, undefined, 2));
-    
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { stat } from 'fs';
+
+// Defines an array of allowed pizza flavors
+const allowedFlavors = ['pepperoni', 'cheese', 'margherita', 'vegetarian'];
+
+// definition of the lambda function
+export const handler: APIGatewayProxyHandler = async (event) => {
+  try {
+    console.log('Received event:', `${event.body} --> type: ${typeof(event.body)}`); // Add this line to log the event
+
+    // Parses the incoming event to get the flavor
+    // Im using .stringify() despite 'event.body' is a string in the unit tests to ensure that event.body is treated as a string 
+    // and using '{}' to handle the case where 'event.body' might be null or undefined.
+    const requestBody = JSON.parse(JSON.stringify(event.body) || '{}');
+    // using this console.log() to make sure the input is in correct format. 
+    console.log('requestBody:', `${requestBody} --> type: ${typeof(requestBody)}`);
+    // Converts to lowercase for case insensitivity for making it easier to look for specific keywords / flavors in the string
+    let flavor = (requestBody || '').toLowerCase(); 
+    // uses regex pattern to select only the pizza flavor from the 
+    flavor = flavor.match(/"flavour":\s*"(.*?)"/)[1];
+    console.log("Requested Pizza Flavor:", flavor);
+
     let containsPineapple = false;
-    
-    if(flavour == 'pineapple' || flavour =='hawaiian'){
-        containsPineapple = true;
+    let statusCode;
+
+    // Check if the flavor contains pineapple or hawaiian keywords
+    if (flavor.includes('pineapple') || flavor.includes('hawaiian')) {
+      containsPineapple = true;
+      statusCode = 500; // Pineapple or Hawaiian is not allowed
+    } else if (allowedFlavors.includes(flavor)) {
+      // Check if the flavor is not in the list of allowed flavors
+      console.log('flavor: ', flavor);
+      statusCode = 200; // Flavor is among permitted values in the 'allowedFlavors' array. 
+    } else {
+      statusCode = 500;
     }
 
-    return {'containsPineapple': containsPineapple}
-}
+    // Simulate pizza creation and delivery
+    const pizzaStatus = await createAndDeliverPizza(containsPineapple);
+
+    // Prepare the response
+    const response = {
+      statusCode: statusCode,
+      body: JSON.stringify({
+        containsPineapple: containsPineapple,
+        pizzaStatus: pizzaStatus,
+      }),
+    };
+    console.log('response: ', response);
+
+    return response;
+  } catch (error: any) {
+    console.error("Error:", error);
+    //return handleErrors(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Internal Server Error",
+      }),
+    };
+  }
+};
+
+// The functions: preparePizza, deliverPizza, and createAndDeliverPizza are used outside of the 
+// lambda function to have greater modularity and reusable code making it easier to maintain and test.
+// The functions preparePizza and deliverPizza are called inside the createAndDeliverPizza, so that 
+// createAndDeliverPizza can later be called inside the lambda function (i.e. 'handler'). 
+
+// Simulated function for pizza preparation time
+// The async keyword indicates that this function will perform asynchronous operations, 
+// and Promise<string> signifies that it will return a promise that resolves to a string.
+const preparePizza = async (): Promise<string> => {
+  // used to specify the duration for which the function will "simulate" pizza preparation.
+  const preparationTimeMs = 10; // 0.010 seconds
+  // creates a new promise. A promise is an object that represents a value which may not be 
+  // available yet but will be resolved or rejected in the future. The resolve parameter is 
+  // a function that will be called when the promise is resolved.
+  return new Promise((resolve) => {
+    // schedules a function to be executed after a specified amount of time. In this case, it's
+    // used to simulate the time it takes to prepare a pizza.
+    setTimeout(() => {
+      // used to "resolve" the promise. When the specified preparationTimeMs (in this case, 
+      // 20 milliseconds) has passed, the promise will be resolved with the specified string, indicating that the pizza is prepared.
+      resolve("Pizza is prepared and ready for delivery.");
+    }, preparationTimeMs);
+  });
+};
+
+// Simulated function for pizza delivery time
+// The async keyword indicates that this function will perform asynchronous operations, 
+// and Promise<string> signifies that it will return a promise that resolves to a string.
+const deliverPizza = async (): Promise<string> => {
+  // used to specify the duration for which the function will "simulate" pizza delivery.
+  const deliveryTimeMs = 20; // 0.02 seconds
+  // creates a new promise. A promise is an object that represents a value which may not be 
+  // available yet but will be resolved or rejected in the future. The resolve parameter is 
+  // a function that will be called when the promise is resolved.
+  return new Promise((resolve) => {
+    // schedules a function to be executed after a specified amount of time. In this case, it's
+    // used to simulate the time it takes to deliver a pizza.
+    setTimeout(() => {
+      // used to "resolve" the promise. When the specified deliveryTimeMs (in this case, 
+      // 20 milliseconds) has passed, the promise will be resolved with the specified string, indicating that the pizza is delivered.
+      resolve("Pizza has been delivered to your doorstep.");
+    }, deliveryTimeMs);
+  });
+};
+
+// Simulated function for pizza creation and delivery
+async function createAndDeliverPizza(containsPineapple: boolean): Promise<string> {
+  // Check if the pizza contains pineapple
+  if (containsPineapple) {
+    return `Sorry, we don't add pineapple to our pizzas.`;
+  } else {
+    // If no pineapple and value is in the flavors array, start pizza preparation and delivery
+    const preparationResult = await preparePizza();
+    const deliveryResult = await deliverPizza();
+    return `${preparationResult} ${deliveryResult} Your pizza is on its way!`;
+  }
+};
--- the-state-machine/typescript/test/the-state-machine.test.ts
+++ the-state-machine/typescript/test/the-state-machine.test.ts
@@ -1,6 +1,9 @@
 import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import TheStateMachine = require('../lib/the-state-machine-stack');
+import { APIGatewayProxyEvent } from 'aws-lambda'; // Import the necessary types
+import { handler } from '../lambda-fns/orderPizza'; // Import your Lambda handler function
+
 
 test('API Gateway Proxy Created', () => {
   const app = new cdk.App();
@@ -59,3 +62,72 @@ test('Order Pizza Lambda Created', () => {
   ));
 });
 
+//Both tests inside the 'describe' method are aimed at testing the 'Pizza Order' lambda function.
+describe('Pizza Order Lambda', () => {
+  // this test, checks if the lambda function executes as expected when using "{ 'flavour': 'pepperoni' }"
+  // as the body of the event. 
+  it('should return a successful response for a valid pizza flavor within the array of options --> pepperoni', async () => {
+    // 'APIGatewayProxyEvent' is a type for defining the data structure of the 'event'.
+    // event: APIGatewayProxyEvent specifies that the event variable should conform to the structure 
+    // defined by APIGatewayProxyEvent.
+    // 'as any' is a type assertion to indicate TypeScript no type checking should be done on this specific expression
+    // It's being used to tell TypeScript that, even though you're assigning an object literal to event, you want TypeScript
+    // to consider it as conforming to the APIGatewayProxyEvent type. The reason for using as any here is likely to suppress 
+    // TypeScript errors related to type mismatches between your object literal and the APIGatewayProxyEvent type.
+    const event: APIGatewayProxyEvent = {
+      body: '{ "flavour": "pepperoni" }', 
+    } as any; 
+    // 'as any' is used to type-assert the empty objects passed as the second and third arguments to the handler function. 
+    // This is used to avoid TypeScript type checking for these objects.
+    const response = await handler(event, {} as any, {} as any) || { statusCode: -1 }; 
+
+    expect(response.statusCode).toBe(200);
+
+    if (response.statusCode === 200 && 'body' in response && response.body !== undefined) {
+      expect(response.body).toBeDefined();
+      // By adding ! after response.body, I'm telling TypeScript to treat response.body as if it's guaranteed to 
+      // be defined and not null. This can be useful when I'm sure that response.body should never be null 
+      // or undefined at this point in the code.
+      // At this point, response.body is not null nor undefined since it already passed the 'response.body !== undefined' condition. 
+      const responseBody = JSON.parse(response.body!);
+      expect(responseBody.containsPineapple).toBe(false);
+      expect(responseBody.pizzaStatus).toContain('Pizza is prepared');
+    }
+  });
+
+  it('should return a successful response for a valid pizza flavor within the array of options --> cheese', async () => {
+    const event: APIGatewayProxyEvent = {
+      body: '{ "flavour": "cheese" }', 
+    } as any; 
+    const response = await handler(event, {} as any, {} as any) || { statusCode: -1 }; 
+
+    expect(response.statusCode).toBe(200);
+
+    if (response.statusCode === 200 && 'body' in response && response.body !== undefined) {
+      expect(response.body).toBeDefined();
+      const responseBody = JSON.parse(response.body!);
+      expect(responseBody.containsPineapple).toBe(false);
+      expect(responseBody.pizzaStatus).toContain('Pizza is prepared');
+    }
+  });
+
+  // this test, checks if the lambda function executes as expected when using "{ 'flavour': 'hawaiian' }" or "{ 'flavour': 'pineapple' }"
+  // as the body of the event. 
+  it('should return an error response for an invalid request', async () => {
+    const event: APIGatewayProxyEvent = {
+      body: '{ "flavour": "pineapple" }', 
+    } as any;
+
+    const response = await handler(event, {} as any, {} as any) || { statusCode: -1 }; // Default value;
+
+    expect(response.statusCode).toBe(500);
+
+    if (response.statusCode === 500 && 'body' in response && response.body !== undefined) {
+      const responseBodyPineapple = JSON.parse(response.body!);
+      expect(responseBodyPineapple.containsPineapple).toBe(true);
+      expect(responseBodyPineapple.pizzaStatus).toContain("Sorry, we don't add pineapple to our pizzas.");
+
+      const responseBody = JSON.parse(response.body!);
+    }
+  });
+});
